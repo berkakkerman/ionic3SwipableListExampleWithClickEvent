ee805a79cc0a9cbe2e8fa81298a1e0bf00dcc5ae
berk <berkakkerman@hotmail.com>
2017-05-28 19:31:00 +0300

    Delete button added with Yes/No Options

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
-import { Http} from '@angular/http';
-import {PeopleServiceProvider} from '../../providers/people-service/people-service';
+import { Http } from '@angular/http';
+import { PeopleServiceProvider } from '../../providers/people-service/people-service';
 
 @Component({
   selector: 'page-home',
@@ -10,30 +10,55 @@ import {PeopleServiceProvider} from '../../providers/people-service/people-servi
 })
 export class HomePage {
 
-  public people:any;
+  public people: any;
   constructor(
     public navCtrl: NavController,
-    public http:Http,
-    public peopleService:PeopleServiceProvider,
-    public alertCtrl:AlertController
-    ) 
-    {
-      this.load();
-    }
-  load(){
-    this.peopleService.load()
-    .then(data=>{
-        this.people=data;
-        console.log('home load() => '+ this.people);
-        
-    });    
+    public http: Http,
+    public peopleService: PeopleServiceProvider,
+    public alertCtrl: AlertController
+  ) {
+    this.load();
   }
-  share(username){
-      let alert = this.alertCtrl.create({
-        title: 'Share Button Clicked',
-        subTitle: username,
-        buttons: ['Dismiss']
+  load() {
+    this.peopleService.load()
+      .then(data => {
+        this.people = data;
+        console.log('home load() => ' + this.people);
+
       });
-      alert.present();
-    }
+  }
+  share(username) {
+    let alert = this.alertCtrl.create({
+      title: 'Share Button Clicked',
+      subTitle: username,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+  removeItem(item) {
+    let alert = this.alertCtrl.create({
+      title: 'Really?',
+      message: 'Do you want to delete this person?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+             for (var i = 0; i < this.people.length; i++) {
+              if (this.people[i] == item) {
+                this.people.splice(i, 1);
+              }
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+   
+  }
 }
